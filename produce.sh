0aeb5cc5717af302fd408ba625b69f220ef59680
Erez Hadad <erezh@il.ibm.com>
2025-06-30 23:13:35 +0300

    Notice on produce.sh

--- produce.sh
+++ produce.sh
@@ -6,3 +6,5 @@ source .env
 
 # Store inference data on SkyStore S3-proxy connected to eu-central-1
 python -m client.produce reduced_tronchetto_array.pt
+
+echo Go back to notebook - notification should have been received
